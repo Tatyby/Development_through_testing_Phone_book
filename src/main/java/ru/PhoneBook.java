ca33a6a12ca4a2d0280f12855b3f74f406527c48
Tatiana <in_sdo@mail.ru>
2023-05-03 11:44:49 +0300

    Stub and tests for add

--- src/main/java/ru/PhoneBook.java
+++ src/main/java/ru/PhoneBook.java
@@ -1,7 +1,21 @@
 package ru;
 
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    public static Map<Person, Integer> phoneBook = new TreeMap<>(Comparator.comparing(Person::getName));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(add("Катя", 200));
+        System.out.println(add("Катя1", 300));
+
+
+    }
+
+    public static int add(String name, int number) {
+        return 0;
+
     }
 }
--- src/test/java/ru/PhoneBookTest.java
+++ src/test/java/ru/PhoneBookTest.java
@@ -1,4 +1,19 @@
 package ru;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    public void addTest(){
+        PhoneBook.add("Света", 102);
+        PhoneBook.add("Света2", 103);
+
+        int result = PhoneBook.add("Света3", 104);
+        int expected=3;
+        Assert.assertEquals(expected,result);
+
+    }
+
 }
