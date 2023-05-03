4f9d3f80ba1faf2e2fb8ed236257efe99aa4b38b
Tatiana <in_sdo@mail.ru>
2023-05-03 11:48:05 +0300

    add implemented

--- src/main/java/ru/PhoneBook.java
+++ src/main/java/ru/PhoneBook.java
@@ -10,12 +10,17 @@ public class PhoneBook {
     public static void main(String[] args) {
         System.out.println(add("Катя", 200));
         System.out.println(add("Катя1", 300));
+        System.out.println(add("Катя2", 300));
+        System.out.println(add("Катя2", 400));
 
 
     }
 
     public static int add(String name, int number) {
-        return 0;
-
+        int countContact;
+        phoneBook.put(new Person(name), number);
+        countContact = phoneBook.size();
+        System.out.print("Количество контактов: ");
+        return countContact;
     }
 }
